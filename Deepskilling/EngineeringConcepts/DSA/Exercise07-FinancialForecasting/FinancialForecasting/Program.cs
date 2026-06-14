bc44904a09160d61dcb3a831b35b2872790ccbf0
Kiran Kurapati <kirankurapati@Kirans-MacBook-Air.local>
2026-06-14 16:40:58 +0530

    Completed DSA Financial Forecasting exercise

---
+++ Deepskilling/EngineeringConcepts/DSA/Exercise07-FinancialForecasting/FinancialForecasting/Program.cs
@@ -0,0 +1,12 @@
+﻿double currentValue = 10000;
+double growthRate = 0.10; // 10%
+int years = 5;
+
+double futureValue =
+    FinancialForecast.PredictFutureValue(
+        currentValue,
+        growthRate,
+        years
+    );
+
+Console.WriteLine($"Future Value after {years} years: {futureValue:F2}");
