0c89a6cbfa50ce085bbf35c75dfae9fd3c8c36a7
Kiran Kurapati <kirankurapati@Kirans-MacBook-Air.local>
2026-06-14 16:23:42 +0530

    Completed Factory Method Pattern Exercise

---
+++ Deepskilling/EngineeringConcepts/DesignPatterns/Exercise02-FactoryMethod/FactoryMethodPatternExample/Program.cs
@@ -0,0 +1,11 @@
+﻿DocumentFactory wordFactory = new WordFactory();
+IDocument wordDoc = wordFactory.CreateDocument();
+wordDoc.Open();
+
+DocumentFactory pdfFactory = new PdfFactory();
+IDocument pdfDoc = pdfFactory.CreateDocument();
+pdfDoc.Open();
+
+DocumentFactory excelFactory = new ExcelFactory();
+IDocument excelDoc = excelFactory.CreateDocument();
+excelDoc.Open();
